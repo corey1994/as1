569563fc66052902cb6b8e2e23b32b3d370f7c17
Corey <hunt1@ualberta.ca>
2016-09-24 22:42:04 -0600

    removed backwards compatibility

---
+++ app/src/main/java/com/example/hunt1_habittracker/AddHabitActivity.java
@@ -0,0 +1,13 @@
+package com.example.hunt1_habittracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddHabitActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_habit);
+    }
+}
